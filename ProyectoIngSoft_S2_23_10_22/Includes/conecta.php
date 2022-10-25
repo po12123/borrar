43cf4cade87b3a255881571f44a6c128211a3a9a
po12123 <58833511+po12123@users.noreply.github.com>
2022-10-25 11:07:08 -0400

    Update conecta.php

--- ProyectoIngSoft_S2_23_10_22/Includes/conecta.php
+++ ProyectoIngSoft_S2_23_10_22/Includes/conecta.php
@@ -1,8 +1,9 @@
 <?php
-$servidor = "db4free.net:3306";
-$usuario = "po12123";
-$password = "contraseña";
-$bd = "notime2waste";
+$servidor = "sql10.freesqldatabase.com:3306";
+$usuario = "sql10529152";
+$password = "L93ygJjdQY";
+$bd = "sql10529152";
+
 $conecta = mysqli_connect($servidor, $usuario,$password,$bd);
 if($conecta->connect_error){
   die("Error al conectar la base de datos de la pagina".$conecta->connect_error);
